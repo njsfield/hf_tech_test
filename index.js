4c52a17693ec400413ebd9c989f9b92d644709dc
Nick Field <heyimnick@live.com>
2017-08-19 13:05:30 +0100

    add main server start file

---
+++ index.js
@@ -0,0 +1,11 @@
+const server  = require('./src');
+const { log } = require('./src/utils');
+const _log = log('SERVER');
+
+// Prevent CORS issues with port 4444;
+const PORT = 4444;
+const HOST = 'http://localhost';
+
+server.listen(PORT, () => {
+  _log(`Server live at ${HOST}:${PORT}/`);
+});
